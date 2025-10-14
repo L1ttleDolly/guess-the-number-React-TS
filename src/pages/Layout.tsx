8481732d16329b7308bbeceda5327ad2c2e55bf0
Vitaliya <vitaliya_malyutina@mail.ru>
2025-10-14 22:51:58 +0300

    Добавлены стили для активно вкладки

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,14 @@
-import {Link, Outlet} from "react-router-dom";
+import { NavLink, Outlet} from "react-router-dom";
 import Header from "../components/Header/Header.tsx";
 
 export default function Layout() {
     return (
         <>
             <Header>
-                <Link to="/" className="header__link">Главная</Link>
-                <Link to="/guess-the-number" className="header__link">Игра угадай число</Link>
-                <Link to="/two-chairs-game" className="header__link">Игра два стула</Link>
-                <Link to="/about-me" className="header__link">Обо мне</Link>
+                <NavLink to="/" className="header__link">Главная</NavLink>
+                <NavLink to="/guess-the-number" className="header__link">Игра угадай число</NavLink>
+                <NavLink to="/two-chairs-game" className="header__link">Игра два стула</NavLink>
+                <NavLink to="/about-me" className="header__link">Обо мне</NavLink>
             </Header>
             <main style={{width: "100%"}}>
                 <Outlet />
