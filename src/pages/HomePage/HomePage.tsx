ff678dbd9da89ddbb887bafa63933e168d3f5542
Vitaliya <vitaliya_malyutina@mail.ru>
2025-10-15 13:29:16 +0300

    Добавлено временное README

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ export default function HomePage() {
     return (
        <section className="home__page-container">
            <h1 className="home__page-title">Пока что здесь пусто!</h1>
+           <p style={{color: "white"}}>Но можешь сыграть в угадай число</p>
            <div className="decor-pepe"></div>
        </section>
     )
