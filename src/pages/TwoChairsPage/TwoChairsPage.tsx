72905d8e2d41c6481e8a76438849b2eecfa6e512
Vitaliya <vitaliya_malyutina@mail.ru>
2025-10-17 01:58:34 +0300

    Добавлена примерная разметка для игры "Два стула"

--- src/pages/TwoChairsPage/TwoChairsPage.tsx
+++ src/pages/TwoChairsPage/TwoChairsPage.tsx
@@ -1,6 +1,22 @@
+import Button from "../../components/Button/Button.tsx";
+import {useState} from "react";
+
 export default function TwoChairsPage() {
+
+    /*const [textArticle, setTextArticle] = useState('')*/
+    const [buttonTextOne, setButtonTextOne] = useState('Первый стул')
+    const [buttonTextTwo, setButtonTextTwo] = useState('Второй стул')
+
+    const showChairOne = () => {
+        console.log('Не нажимай на меня')
+    }
+
+    const showChairTwo = () => {
+        console.log('Второй стул')
+    }
+
     return (
-        <div className="container">
+      /*  <div className="container">
             <h2 className="title">
                 Скоро здесь появится игра...
             </h2>
@@ -8,8 +24,44 @@ export default function TwoChairsPage() {
                     Пока держите котика :3
                 </p>
                     <div className="decor-cat"></div>
+        </div>*/
+        <>
+            <section className="container">
+                <div className="container-start">
+                    <Button className="button-start">
+                        Начать
+                    </Button>
+                </div>
+                <div className="questions-container">
+                    В разработке!
+                    <div className="decor-cat"></div>
+                </div>
+                <div className="chairs-container">
 
+                    <Button
+                        onClick={showChairOne}
+                        className="chair"
+                    >
+                        {buttonTextOne}
+                    </Button>
 
-        </div>
+                    <Button
+                        onClick={showChairTwo}
+                        className="chair"
+                    >
+                        {buttonTextTwo}
+                    </Button>
+
+                    <Button
+                        onClick={showChairOne}
+                        className="chair"
+                    >
+                        {buttonTextOne}
+                    </Button>
+                </div>
+
+            </section>
+
+        </>
     )
 }
